0218ad6ebd2316944f75d6c69fa4c27047e94f67
Tanish Sabne <161741259+TanishSabne@users.noreply.github.com>
2024-06-21 16:36:50 +0530

    Update burp.sh

--- burp.sh
+++ burp.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+burp_url="https://portswigger.net/burp/releases/startdownload?product=community&version=2024.5.3&type=Linux"
+
+wget -q --show-progress -O "burp.sh" "$burp_url"
+printf "o\n\ny\n\n" | sh burp.sh
+
+log "Installation Successfull"
